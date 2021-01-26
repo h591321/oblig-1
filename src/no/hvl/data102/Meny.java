b30b58bb2c1f379600c9d505e1ef966f8957b3ab
yfess <yfess@158.37.226.248>
2021-01-26 17:22:46 +0100

    lit intro på ment klassen, fikk hjelp av studas, men sjenner ikke helt hva jeg skal gjøre

--- src/no/hvl/data102/Meny.java
+++ src/no/hvl/data102/Meny.java
@@ -1,10 +1,35 @@
 package no.hvl.data102;
 
+import no.hvl.data102.adt.FilmarkivADT;
+
 public class Meny {
-
-	Meny(FilmarkivADT filma){
-		
+	
+	private Tekstgrensesnitt tekstgrensesnitt;
+	private FilmarkivADT filmarkiv;
+	
+	public Meny(FilmarkivADT filmer) {
+		tekstgrensesnitt= new Tekstgrensesnitt();
+		filmarkiv = filmer;
 	}
+	
+	 public void start(){
+		 int valg=valg();
+		 switch (valg) {
+		 case 1: 
+		 
+		 
+		 
+		 }
+		 
+		 
+	 }
 
-
+	 
+	 private int valg() {
+		 int valget=0;
+		 System.out.println("1) ei velkommer til dette");
+		 System.out.println("2) ei velkommer til dette");
+		 
+		 return valget;
+	 }
 }
