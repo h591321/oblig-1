7edef1eacf516108bea694e75e33d6f46ce5986e
yfess <yfess@DESKTOP-MO3P69F>
2021-02-11 16:51:35 +0100

    ferdig Testing

--- TestKoe/adt/KoeADTTest.java
+++ TestKoe/adt/KoeADTTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -39,7 +40,7 @@ public abstract class KoeADTTest {
 		 * Test på at en ny Koe er tom.
 		 */
 		@Test
-		public void nyKoeErTom() {
+		public void nyKoeErTomTest() {
 			assertTrue(Koe.erTom());
 		}
 		
@@ -49,7 +50,7 @@ public abstract class KoeADTTest {
 		 * Test på at en Koe med noen elementer ikke er tom.
 		 */
 		@Test
-		public final void erIkkeTom() {
+		public final void erIkkeTomTest() {
 			Koe.innKoe(e0);
 			Koe.innKoe(e1);
 			assertFalse(Koe.erTom());
@@ -62,7 +63,18 @@ public abstract class KoeADTTest {
 		 * Test på at en Koe med null elementer er tom.
 		 */
 		@Test
-		public void innKoeUtKoeTom() { 
+		public void innKoeUtKoeTomTest() { 
+				Koe.innKoe(e0);
+				assertEquals(e0,Koe.foerste());
+		}
+		
+		
+
+		/**
+		 * Tester første element i Koen
+		 */
+		@Test
+		public void foersteTest() { 
 			try {
 				Koe.innKoe(e0);
 				Koe.utKoe();
@@ -79,7 +91,7 @@ public abstract class KoeADTTest {
 		 * Test opå innKoe and UtKoe.
 		 */
 		@Test
-		public void innKoeOgUttKoe() {
+		public void innKoeOgUttKoeTest() {
 
 			Koe.innKoe(e0);
 			Koe.innKoe(e1);
@@ -94,6 +106,38 @@ public abstract class KoeADTTest {
 			} catch (EmptyCollectionException e) {
 				fail("pop feilet uventet " + e.getMessage());
 			}
+		}
+		
+		
+		
+		/**
+		 * Test Koe størrelsen
+		 */
+		@Test
+		public void antallTest() {
+			Koe.innKoe(e0);
+			Koe.innKoe(e1);
+			Koe.innKoe(e2);
+			assertEquals(3, Koe.antall());
+		}
+		
+		
+		/**
+		 * Forsøk på uttKoe av en tom Koe skal gi "underflow excepton" *
+		 * 
+		 * @throws EmptyCollectionException expected exception
+		 */
+		@Test
+		public void UttKoeFromEmptyIsUnderflowed() {
+			/*
+			 * Assertions.assertThrows(EmptyCollectionException.class, new Executable() {
+			 * 
+			 * @Override public void execute() throws Throwable { stabel.pop(); } });
+			 */
+
+			Assertions.assertThrows(EmptyCollectionException.class, () -> {
+				Koe.utKoe();
+			});
 		}
 
 }
